8cb8f054b6a776d9dae1998cabb46db81acc9119
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-10-02 09:27:46 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ for(let i=0;i<arr.length;i++)
         if(y%2===0)
             arr2[i].push(arr[i].slice(y,y+2))
     }
-     arr2.map(a=>a.map(b=>parseInt(b).filter(c=>{if(c===10)return "."; else if(c===11)return "-"}));
+     arr2.map(a=>a.map(b=>parseInt(b).filter(c=>{if(c===10)return "."; else if(c===11)return "-"})));
               console.log(arr2)
 }
 
