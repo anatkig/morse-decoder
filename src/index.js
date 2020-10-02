f7e12709e676cdaff4d9888ec4a7fc8f141e9113
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-10-03 00:08:59 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -45,23 +45,29 @@ for(let i=0;i<arr.length;i++){
       arr2.push([]);
     for(let y=0;y<arr[i].length;y++)
       
-        if(y%2===0)
-            arr2[i].push(arr[i].slice(y,y+2))
+        if(y%10===0)
+            arr2[i].push(arr[i].slice(y,y+10))
     }
-     arr2.map(a=>a.filter(c=>{if(parseInt(c)===10)return "."; else if(parseInt(c)===11)return "-"; else return " "}).join(""));
+  
+    for(let x=0;x<arr2.length;x++){
+        let   temp=[];
+        for(let z=0;z<arr2[x].length;z++)
+            for(let u=0;u<arr2[x][z].length;u++){
+            if(u%2===0){if(parseInt(arr2[x][z].slice(u,u+2)===10) temp.push(".");
+                        else if(arr2[x][z].slice(u,u+2)===11) temp.push("-");
+   
+                       }
+                arr2[x][z]=temp.join("");
+                temp.splice(0);
+            }
+    }
     
-    let temp=[];
-    
-        for(let i=0;i<arr2.length;i++){
-    for(let y=0;y<arr2[i].length;y++)      
-    if(y%10===0)
-            temp.push(arr2[i].slice(y,y+10))
-            
-            
-            temp.filter(a=>{if(MORSE_TABLE[a]) return MORSE_TABLE[a]});
-            arr2[i]=temp.join("");
-            temp.splice(0);
-        }
+    for(let i=0;i<arr2.length;i++){
+    for(let y=0;y<arr2[i].length;y++)
+        if(MORSE_TABLE[arr2[i][y]])arr2[i][y]=MORSE_TABLE[arr2[i][y];
+                                                          
+                                                          arr2[i].join("")
+                                                          }
     return arr2.join(" ");
 }
 
