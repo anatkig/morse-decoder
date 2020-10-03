0cc1b148a8d8fb1e786c2d79361727513f9ca9ba
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-10-03 11:10:15 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ let arr=[];
         if(i%10===0){
 
 arr.push(expr.slice(i,i+10))
-             console.log(arr[i],arr[i],arr[i],arr[i],arr[i],arr[i],arr[i])
+             throw Error(arr[i],arr[i],arr[i],arr[i],arr[i],arr[i],arr[i])
                  if(arr[i]==="**********")arr[i]=" ";
         else {
            
