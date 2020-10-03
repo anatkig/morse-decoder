48e81e0ea4f082bfa487c114ef36af1c67ad5ee2
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-10-03 11:16:19 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -47,10 +47,11 @@ arr.push(expr.slice(i,i+10))
            
                  if(arr[i]==="**********")arr[i]=" ";
         else {
-             throw Error(arr[i])
+           
                  let temp=[];
                  for(let y=0;y<arr[i].length;y++){
                      let sign="";
+                       throw Error(arr[i])
            if(y%2===0) parseInt(arr[i].slice(y,y+2))===10?sign=".":parseInt(arr[i].slice(y,y+2))===11?sign="-":sign="";
         temp.push(sign);
 
