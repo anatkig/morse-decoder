a7d928f1856e8820b0d953c7c3e5d1679d93a600
Anatoliy <70736455+anatkig@users.noreply.github.com>
2020-10-03 14:15:33 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -42,32 +42,10 @@ function decode(expr) {
 let arr=[];
     for(let i=0;i<expr.length;i++){
         if(i%10===0){
-
 arr.push(expr.slice(i,i+10))
-           
-                 if(arr[i]==="**********")arr[i]=" ";
-        else {
-           
-                 let temp=[];
-            if(arr[i])
-                 for(let y=0;y<arr[i].length;y++){
-                     let sign="";
-                     
-           if(y%2===0) {arr[i].slice(y,y+2)==="10"?sign=".":arr[i].slice(y,y+2)==="11"?sign="-":sign="";
-        temp.push(sign);}
-
- 
-    }//for y
-            
-            arr[i]=temp.join("");
-            throw Error(arr[i])
-           
-        }//else
-        }//if
-  }//for i
-    
-    arr.map(a=>{return MORSE_TABLE[a]})
-    return arr.join("").split(" ").join(" ");
+        }
+    }//for       
+    throw Error(arr)
 }
 
 
